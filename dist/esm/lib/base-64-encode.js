89dc383c068f13518e5d79d1b4579e292a0d0017
Martin Baker <mabaker@hq.bill.com>
2023-04-18 20:54:04 -0700

    Adding dist from published @segment/analytics-node

---
+++ dist/esm/lib/base-64-encode.js
@@ -0,0 +1,8 @@
+import { Buffer } from 'buffer';
+/**
+ * Base64 encoder that works in browser, worker, node runtimes.
+ */
+export const b64encode = (str) => {
+    return Buffer.from(str).toString('base64');
+};
+//# sourceMappingURL=base-64-encode.js.map
